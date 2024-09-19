852098007e9d6ce94f68a48d82981329ece28050
besnikqunaku <b.qunaku7@gmail.com>
2024-09-19 19:39:58 +0200

    Moved out the business logic in the services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,28 +8,25 @@ import {
   Body,
   HttpCode,
 } from '@nestjs/common';
-import { v4 as uuid } from 'uuid';
-import { data, ReportType } from './data';
+import { ReportType } from './data';
+import { AppService } from './app.service';
 
 @Controller('report/:type')
 export class AppController {
+  constructor(private readonly appServie: AppService) {}
+
   @Get()
   getAllReports(@Param('type') type: string) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    return data.report.filter((report) => report.type === reportType);
+    return this.appServie.getAllReports(reportType);
   }
 
   @Get(':id')
   getReportById(@Param('type') type: string, @Param('id') id: string) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    const filteredData = data.report.filter(
-      (report) => report.type === reportType,
-    );
-    const report = filteredData.find((item) => item.id === id);
-    if (!report) return 'Sorry no report founded!';
-    return report;
+    return this.appServie.getReport(id, reportType);
   }
 
   @Post()
@@ -39,16 +36,7 @@ export class AppController {
   ) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    const newReport = {
-      id: uuid(),
-      amount: amount,
-      source: source,
-      created_at: new Date(),
-      updated_at: new Date(),
-      type: reportType,
-    };
-    data.report.push(newReport);
-    return newReport;
+    return this.appServie.createReport(reportType, amount, source);
   }
 
   @Put(':id')
@@ -59,30 +47,12 @@ export class AppController {
   ) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    const filteredData = data.report.filter((item) => item.type === reportType);
-    const reportToUpdated = filteredData.find((item) => item.id === id);
-
-    if (!reportToUpdated) return 'Sorry report not founded!';
-
-    const reportIndex = data.report.findIndex(
-      (report) => report.id === reportToUpdated.id,
-    );
-
-    data.report[reportIndex] = {
-      ...data.report[reportIndex],
-      amount,
-      source,
-    };
-
-    return data.report[reportIndex];
+    return this.appServie.updateReport(reportType, id, amount, source);
   }
 
   @HttpCode(204)
   @Delete(':id')
   deleteReport(@Param('id') id: string) {
-    const reportIndex = data.report.findIndex((report) => report.id === id);
-    if (reportIndex === -1) return 'Sorry report not founded!';
-    data.report.splice(reportIndex, 1);
-    return 'Deleted';
+    this.appServie.deleteReport(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { ReportType, data } from './data';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAllReports(type: ReportType) {
+    return data.report.filter((report) => report.type === type);
+  }
+
+  getReport(id: string, type: ReportType) {
+    const report = data.report
+      .filter((report) => report.type === type)
+      .find((item) => item.id === id);
+
+    if (!report) {
+      throw new NotFoundException('Report not found');
+    }
+
+    return report;
+  }
+
+  createReport(type: ReportType, amount: number, source: string) {
+    const newReport = {
+      id: uuid(),
+      amount,
+      source,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type,
+    };
+
+    data.report.push(newReport);
+    return newReport;
+  }
+
+  updateReport(type: ReportType, id: string, amount: number, source: string) {
+    const reportIndex = data.report.findIndex(
+      (report) => report.id === id && report.type === type,
+    );
+
+    if (reportIndex === -1) {
+      throw new NotFoundException('Report not found');
+    }
+
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      amount,
+      source,
+      updated_at: new Date(),
+    };
+
+    return data.report[reportIndex];
+  }
+
+  deleteReport(id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+
+    if (reportIndex === -1) {
+      throw new NotFoundException('Report not found');
+    }
+
+    data.report.splice(reportIndex, 1);
+    return;
   }
 }
